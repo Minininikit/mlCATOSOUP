829745eb15d2210d79eeba55764396822dbd708a
Minininikit <minifaking@gmail.com>
2024-05-14 20:36:27 +0300

    changed test_size to 0.1

--- Classifier.py
+++ Classifier.py
@@ -21,7 +21,7 @@ def dataPre(data):
     data_features = ['A','B', 'C']
     X = data[data_features]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
     return X_train, X_test, y_train, y_test
 
